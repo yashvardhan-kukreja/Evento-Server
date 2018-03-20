b0a4e637b79af9b3f832fafb83cd5c253f9d1eeb
Yashvardhan-Kukreja <yash.kukreja.98@gmail.com>
2018-03-21 00:29:26 +0530

    Initialised the main server script

---
+++ app.js
@@ -0,0 +1,44 @@
+/**
+ * Created by Yash 1300 on 21-03-2018.
+ */
+
+const mongoose = require('mongoose');
+const logger = require('morgan');
+const bodyParser = require('body-parser');
+const express = require('express');
+const helmet = require('helmet');
+const compression = require('compression');
+
+const config = require('./config');
+
+const app = express();
+const port = process.env.PORT || 8000;
+const db = config.DATABASE;
+
+
+// Establishing connection to the database
+mongoose.connect(db, function(err){
+    if (err) {
+        console.log("Error connecting the database");
+    } else {
+        console.log("Database connected successfully...");
+    }
+});
+
+// Attaching logger to the app
+app.use(logger('dev'));
+
+// Attaching body parser to the app to read request bodies
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
+
+// Attaching "helmet" to the app to secure various HTTP headers
+// Attaching "compression" to compress the requests passing through middle wares
+app.use(helmet());
+app.use(compression());
+
+
+// Starting the server
+app.listen(port, function(){
+    console.log("App running successfully on port number: " + port + "...");
+});
---
+++ config.js
@@ -0,0 +1,14 @@
+/**
+ * Created by Yash 1300 on 21-03-2018.
+ */
+
+// These variable are just for testing.
+/*
+While deploying the back end to the cloud,
+these variables will be removed from here and will be involved in the environment variables of the cloud with different values
+*/
+
+module.exports = {
+    DATABASE: "",
+    SECRET: "luci4"
+};
