534dfced0bd97950f895ebf21ec1ecd9007de09d
Yashvardhan-Kukreja <yash.kukreja.98@gmail.com>
2018-03-27 05:07:18 +0530

    Corrected the user registeration function returning promise

--- controllers/auth_controller.js
+++ controllers/auth_controller.js
@@ -4,18 +4,27 @@
 const Promise = require('bluebird');
 const User = require('../database/users/userSchema');
 
-module.exports.get_register = (email, password) => {
-    return new Promise((resolve) => {
-             var user = new User({email : email, password: password});
-             user.save((err) => {
-                 if(err) return res.status(200).json({
-                    status: 400,
-                    message: 'Failed to insert data',
-                    info: err
-                });
-                if(status(200)){
-                    return resolve({status: 200, message: 'Sucessfully added new user'});
-                }
+// Function returning promise for executing the registration of a user
+module.exports.register = (name, username, email, password, contact) => {
+    return new Promise((resolve, reject) => {
+        var user = new User({name: name, username: username, email: email, password: password, contact: contact});
+
+        // Checking if the user has completely filled all the fields
+        if (name.trim() === "" || name.trim() === null || username.trim() === "" || username.trim() === null || email.trim() === "" || email.trim() === null || password.trim() === "" || password.trim() === null || contact.trim() === "" || contact.trim() === null)
+            return reject({success: false, message: "Please enter all the fields"});
+        else {
+
+            // Saving the user
+            user.save((err) => {
+                if(err) {
+                    console.log(err);
+                    if (err.code === 11000)
+                        return reject({success: false, message: "A user already exists with the given username or email"});
+                    else
+                        return reject({success: false, message: "An error occurred while signing up"});
+                } else
+                    return resolve({success: true, message: "User registred successfully"});
             });
+        }
     });
 };
