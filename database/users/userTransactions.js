78772dc2d311370070784f63f8d38cb87bed726e
Yashvardhan-Kukreja <yash.kukreja.98@gmail.com>
2018-03-27 07:32:20 +0530

    Changed the structure a bit

--- database/users/userTransactions.js
+++ database/users/userTransactions.js
@@ -1,13 +1,12 @@
 /**
  * Created by Yash 1300 on 21-03-2018.
  */
-const mongoose = require('mongoose');
 const bcrypt = require('bcrypt-nodejs');
 const jwt = require('jsonwebtoken');
 const User = require('./userSchema');
 
 // Function returning a user object corresponding to the provided username or email
-function findUserByUsernameOrEmail(input) {
+module.exports.findUserByUsernameOrEmail = (input) => {
     User.findOne({$or: [{username: input}, {email: input}]}).exec((err, outputUser) => {
         if (err)
             return false;
@@ -18,22 +17,14 @@ function findUserByUsernameOrEmail(input) {
                 return outputUser;
         }
     });
-}
+};
 
 // Function for comparing password for a user object
-function comparePassword(user, password) {
+module.exports.comparePassword = (user, password) => {
     return bcrypt.compareSync(password, user.password)
-}
+};
 
 // Function for generating a token for a user object
-function generateToken(user, secret) {
+module.exports.generateToken = (user, secret) => {
     return jwt.sign(JSON.parse(JSON.stringify(user)), secret);
-}
-
-
-module.exports = {
-    findUserByUsernameOrEmail: findUserByUsernameOrEmail,
-    comparePassword: comparePassword,
-    generateToken: generateToken
-
 };
