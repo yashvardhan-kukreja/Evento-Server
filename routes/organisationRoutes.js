7c5366de5013f8bfad4d1590114f0709c91894f8
Yashvardhan-Kukreja <yash.kukreja.98@gmail.com>
2018-03-29 03:49:15 +0530

    Attached token verification to the router

--- routes/organisationRoutes.js
+++ routes/organisationRoutes.js
@@ -1,3 +1,15 @@
 /**
  * Created by Yash 1300 on 29-03-2018.
  */
+
+const router = require('express').Router();
+const Organisation = require('../database/organisations/organisationSchema');
+const organisationTransations = require('../database/organisations/organisationTransactions');
+
+const secret = process.env.SECRET;
+
+router.use((req, res, next) => {
+    organisationTransations.verifyOrganisationToken(secret, req, res, next);
+});
+
+module.exports = router;
--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,3 +1,15 @@
 /**
  * Created by Yash 1300 on 29-03-2018.
  */
+
+const router = require('express').Router();
+const User = require('../database/users/userSchema');
+const userTransations = require('../database/users/userTransactions');
+
+const secret = process.env.SECRET;
+
+router.use((req, res, next) => {
+    userTransations.verifyUserToken(secret, req, res, next);
+});
+
+module.exports = router;
