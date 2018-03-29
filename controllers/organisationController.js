57bf5a1a36746d0c16a32ac5899c06143abf96cd
Yashvardhan-Kukreja <yash.kukreja.98@gmail.com>
2018-03-30 04:26:01 +0530

    Added a promise returning an organisation's details on the basis of id

--- controllers/organisationController.js
+++ controllers/organisationController.js
@@ -1,3 +1,23 @@
 /**
  * Created by Yash 1300 on 29-03-2018.
  */
+
+const Promise = require('bluebird');
+const organisationTransactions = require('../database/organisations/organisationTransactions');
+
+module.exports.getOrganisationDetails = (id) => {
+    return new Promise((resolve, reject) => {
+        organisationTransactions.findOrganisationById(id, (err, outputOrganisation) => {
+            if (err) {
+                console.log(err);
+                reject({success: false, message: "An error occurred"});
+            } else {
+                if (!outputUser)
+                    reject({success: false, message: "Problem fetching organisation details"});
+                else {
+                    resolve({success: true, message: "Organisation details fetched successfully", organisation: outputOrganisation});
+                }
+            }
+        });
+    });
+};
