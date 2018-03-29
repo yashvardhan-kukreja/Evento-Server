64ab6d1a4075860be236a3b8dca9674a68f8f910
Yashvardhan-Kukreja <yash.kukreja.98@gmail.com>
2018-03-30 04:07:36 +0530

    Added a promise to return the user details on the basis of id

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,3 +1,24 @@
 /**
  * Created by Yash 1300 on 29-03-2018.
  */
+
+
+const Promise = require('bluebird');
+const userTransactions = require('../database/users/userTransactions');
+
+module.exports.getUserDetails = (id) => {
+    return new Promise((resolve, reject) => {
+        userTransactions.findUserById(id, (err, outputUser) => {
+            if (err) {
+                console.log(err);
+                reject({success: false, message: "An error occurred"});
+            } else {
+                if (!outputUser)
+                    reject({success: false, message: "Problem fetching user details"});
+                else {
+                    resolve({success: true, message: "User details fetched successfully", user: outputUser});
+                }
+            }
+        });
+    });
+};
