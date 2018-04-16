ac7db1ec851056978d073c4b9edbc0db4122e1e8
Yashvardhan-Kukreja <yash.kukreja.98@gmail.com>
2018-04-16 16:00:15 +0530

    Removed the unnecessary declaration of bcrypt

--- database/users/userSchema.js
+++ database/users/userSchema.js
@@ -2,7 +2,6 @@
  * Created by abhi on 21-Mar-18.
  */
 const mongoose  = require('mongoose');
-const bcrypt = require('bcrypt-nodejs');
 const Schema    = mongoose.Schema;
 
 const userSchema = new Schema({
