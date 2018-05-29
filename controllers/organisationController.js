ab064fd05e2819eb9ea46c969222c8a1baf13a7a
Yashvardhan-Kukreja <yash.kukreja.98@gmail.com>
2018-05-29 12:43:50 +0530

    Added controllers for hosting and deleting an event

--- controllers/organisationController.js
+++ controllers/organisationController.js
@@ -3,11 +3,12 @@
  */
 
 const Promise = require('bluebird');
-const organisationTransactions = require('../database/organisations/organisationTransactions');
+const OrganisationTransactions = require('../database/organisations/organisationTransactions');
+const EventTransactions = require('../database/events/eventTransactions');
 
 module.exports.getOrganisationDetails = (id) => {
     return new Promise((resolve, reject) => {
-        organisationTransactions.findOrganisationById(id, (err, outputOrganisation) => {
+        OrganisationTransactions.findOrganisationById(id, (err, outputOrganisation) => {
             if (err) {
                 console.log(err);
                 reject({success: false, message: "An error occurred"});
@@ -16,6 +17,44 @@ module.exports.getOrganisationDetails = (id) => {
                     reject({success: false, message: "Problem fetching organisation details"});
                 else {
                     resolve({success: true, message: "Organisation details fetched successfully", organisation: outputOrganisation});
+                }
+            }
+        });
+    });
+};
+
+module.exports.hostAnEvent = (name, date, location, organisation_id) => {
+    return new Promise((resolve, reject) => {
+        EventTransactions.addAnEvent(name, date, location, organisation_id, (err) => {
+            if (err) {
+                console.log(err);
+                reject({success: false, message: "An error occurred"});
+            } else
+                resolve({success: true, message: "Event hosted successfully"});
+        });
+    });
+};
+
+module.exports.deleteAnEvent = (event_id, organisation_id) => {
+    return new Promise((resolve, reject) => {
+        EventTransactions.findEventByEventId(event_id, (err, outputEvent)=> {
+            if (err) {
+                console.log(err);
+                reject({success: false, message: "An error occurred while removing the event"});
+            } else {
+                if (!outputEvent)
+                    reject({success: false, message: "Wrong event id entered"});
+                else {
+                    if (outputEvent.hostingOrganisation._id ==  organisation_id) {
+                        EventTransactions.deleteEventByEventId(event_id, (err) => {
+                            if (err) {
+                                console.log(err);
+                                reject({success: false, message: "An error occurred while removing the event"});
+                            } else
+                                resolve({success: true, message: "Event removed successfully"});
+                        });
+                    } else
+                        reject({success: false, message: "Not authorized to delete this event"});
                 }
             }
         });
