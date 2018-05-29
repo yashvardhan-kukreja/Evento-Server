acce7cd6d8d8dd33cad35eb01c6e631d558ec2a5
Yashvardhan-Kukreja <yash.kukreja.98@gmail.com>
2018-05-29 12:55:55 +0530

    Added the route for fetching the event info

---
+++ routes/eventRoutes.js
@@ -0,0 +1,13 @@
+/**
+ * Created by Yash 1300 on 28-05-2018.
+ */
+
+const router = require('express').Router();
+const EventController = require('../controllers/eventController');
+
+router.post('/fetch/info', (req, res) => {
+    const event_id = req.body.event_id;
+    EventController.fetchEventDetails(event_id).then(data => res.json(data)).catch(err => res.json(err));
+});
+
+module.exports = router;
