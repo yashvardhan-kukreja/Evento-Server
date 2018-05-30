cdf0ad3d62079525d494b08ffdce7c8d19f58094
Yashvardhan-Kukreja <yash.kukreja.98@gmail.com>
2018-05-30 15:14:26 +0530

    Added transation for adding and fetching FAQs of an event

--- database/events/eventTransactions.js
+++ database/events/eventTransactions.js
@@ -45,3 +45,19 @@ module.exports.addUserToAnEvent = (event_id, user_id, next) => {
 module.exports.fetchParticipatedEvents = (user_id, next) => {
     Event.find({participants: user_id}, {participants: 0}).populate({path: 'hostingOrganisation', model: 'Organisation'}).exec(next);
 };
+
+module.exports.addFaqs = (event_id, faqs, next) => {
+    Event.findOneAndUpdate({_id: event_id}, {$push: {$each: faqs}}).exec(next);
+};
+
+module.exports.addASingleFAQ = (event_id, question, answer, next) => {
+    let faq = {
+        question: question,
+        answer: answer
+    };
+    Event.findOneAndUpdate({_id: event_id}, {$push: {faqs: faq}}).exec(next);
+};
+
+module.exports.fetchFaqs = (event_id, next) => {
+    Event.findOne({_id: event_id}, 'faqs').exec(next);
+};
