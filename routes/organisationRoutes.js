d7f9e37e7717bda8d5645a677aa145f465da09fb
Yashvardhan-Kukreja <yash.kukreja.98@gmail.com>
2018-05-30 18:00:26 +0530

    Added routes for modifying about, adding fees and adding speakers to the event

--- routes/organisationRoutes.js
+++ routes/organisationRoutes.js
@@ -29,7 +29,10 @@ router.post('/host-event', (req, res) => {
     let name = req.body.event_name;
     let date = req.body.event_date;
     let location = req.body.event_location;
-    OrganisationController.hostAnEvent(name, date, location, organisation_id).then(data => res.json(data)).catch(err => res.json(err));
+    let reg_fees_amount = req.body.reg_fees_amount;
+    let reg_fees_description = req.body.reg_fees_description;
+    let about = req.body.about;
+    OrganisationController.hostAnEvent(name, date, location, organisation_id, reg_fees_amount, reg_fees_description, about).then(data => res.json(data)).catch(err => res.json(err));
 });
 
 // Route for deleting an event
@@ -43,7 +46,7 @@ router.post('/delete-event', (req, res) => {
 });
 
 // Route for adding multiple FAQs in one go
-router.post('/add-multiple-faqs', (req, res) => {
+router.post('/event/add-multiple-faqs', (req, res) => {
     let organisation_id = req.decoded._id;
     let questions = req.body.questions;
     let answers = req.body.answers;
@@ -55,13 +58,62 @@ router.post('/add-multiple-faqs', (req, res) => {
 });
 
 // Route for adding a single FAQ
-router.post('/add-faq', (req, res) => {
+router.post('/event/add-faq', (req, res) => {
     let organisation_id = req.decoded._id;
     let question = req.body.question;
     let answer = req.body.answer;
     let event_id = req.body.event_id;
     OrganisationController.authorizeOrganisationForAnEvent(event_id, organisation_id)
         .then(ifAuthorized => OrganisationController.addSingleFaqToAnEvent(event_id, question, answer))
+        .then(data => res.json(data))
+        .catch(err => res.json(err));
+});
+
+// Route for adding multiple speakers in one go
+router.post('/event/add-multiple-speakers', (req, res) => {
+    let organisation_id = req.decoded._id;
+    let names = req.body.names;
+    let descriptions = req.body.descriptions;
+    let img_urls = req.body.img_urls;
+    let event_id = req.body.event_id;
+    OrganisationController.authorizeOrganisationForAnEvent(event_id, organisation_id)
+        .then(ifAuthorized => OrganisationController.addSpeakersToAnEvent(event_id, names, descriptions, img_urls))
+        .then(data => res.json(data))
+        .catch(err => res.json(err));
+});
+
+// Route for adding a single speaker
+router.post('/event/add-speaker', (req, res) => {
+    let organisation_id = req.decoded._id;
+    let name = req.body.name;
+    let description = req.body.description;
+    let img_url = req.body.img_url;
+    let event_id = req.body.event_id;
+    OrganisationController.authorizeOrganisationForAnEvent(event_id, organisation_id)
+        .then(ifAuthorized => OrganisationController.addSingleSpeakerToAnEvent(event_id, name, description, img_url))
+        .then(data => res.json(data))
+        .catch(err => res.json(err));
+});
+
+// Route for adding fees to the event
+router.post('/event/add-fees', (req, res) => {
+    let organisation_id = req.decoded._id;
+    let amount = req.body.amount;
+    let description = req.body.description;
+    let event_id = req.body.event_id;
+    OrganisationController.authorizeOrganisationForAnEvent(event_id, organisation_id)
+        .then(ifAuthorized => OrganisationController.addFeesToTheEvent(event_id, amount, description))
+        .then(data => res.json(data))
+        .catch(err => res.json(err));
+});
+
+// Route for modifying the 'about' of the event
+router.post('/event/modify-about', (req, res) => {
+    let organisation_id = req.decoded._id;
+    let about = req.body.about;
+    let event_id = req.body.event_id;
+    OrganisationController.authorizeOrganisationForAnEvent(event_id, organisation_id)
+        .then(ifAuthorized => OrganisationController.modifyAboutOfTheEvent(event_id, about))
         .then(data => res.json(data))
         .catch(err => res.json(err));
 });
