a6ad8b191b4827814de92c37250f4795ce777788
Yashvardhan-Kukreja <yash.kukreja.98@gmail.com>
2018-05-30 15:15:47 +0530

    Added route for fetching the faqs of an event

--- routes/eventRoutes.js
+++ routes/eventRoutes.js
@@ -5,9 +5,16 @@
 const router = require('express').Router();
 const EventController = require('../controllers/eventController');
 
+// Route for fetching the event info
 router.post('/fetch/info', (req, res) => {
     let event_id = req.body.event_id;
     EventController.fetchEventDetails(event_id).then(data => res.json(data)).catch(err => res.json(err));
+});
+
+// Route for fetching the FAQs of an event
+router.post('/fetch/faqs', (req, res) => {
+    let event_id = req.body.event_id;
+    EventController.fetchFaqs(event_id).then(data => res.json(data)).catch(err => res.json(err));
 });
 
 module.exports = router;
