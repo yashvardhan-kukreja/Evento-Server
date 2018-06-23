4035d583c8ac60d9ee40a870f4897c70034ea528
Yashvardhan-Kukreja <yash.kukreja.98@gmail.com>
2018-06-23 13:40:04 +0530

    changed _id to eventId in event related queries

--- database/events/eventTransactions.js
+++ database/events/eventTransactions.js
@@ -9,7 +9,7 @@ module.exports.findEventByEventName = (event_name, next) => {
 };
 
 module.exports.findEventByEventId = (event_id, next) => {
-    Event.findOne({_id: event_id}, {participants: 0}).populate({path: 'hostingOrganisation', model: 'Organisation'}).exec(next);
+    Event.findOne({eventId: event_id}, {participants: 0}).populate({path: 'hostingOrganisation', model: 'Organisation'}).exec(next);
 };
 
 module.exports.addAnEvent = (name, start_date, end_date, location, organisation_id, reg_fees, about, pointOfContacts, next) => {
@@ -28,11 +28,11 @@ module.exports.addAnEvent = (name, start_date, end_date, location, organisation_
 };
 
 module.exports.findParticipantsOfAnEvent = (event_id, next) => {
-    Event.findOne({_id: event_id}, 'participants').populate({path: 'participants', model: 'User'}).exec(next);
+    Event.findOne({eventId: event_id}, 'participants').populate({path: 'participants', model: 'User'}).exec(next);
 };
 
 module.exports.findParticipantIdsOfAnEvent = (event_id, next) => {
-    Event.findOne({_id: event_id}, 'participants').exec(next);
+    Event.findOne({eventId: event_id}, 'participants').exec(next);
 };
 
 module.exports.deleteEventByEventName = (event_name, next) => {
@@ -40,11 +40,11 @@ module.exports.deleteEventByEventName = (event_name, next) => {
 };
 
 module.exports.deleteEventByEventId = (event_id, next) => {
-    Event.findOneAndRemove({_id: event_id}).exec(next);
+    Event.findOneAndRemove({eventId: event_id}).exec(next);
 };
 
 module.exports.addUserToAnEvent = (event_id, user_id, next) => {
-    Event.findOneAndUpdate({_id: event_id}, {$push: {participants: user_id}}).exec(next);
+    Event.findOneAndUpdate({eventId: event_id}, {$push: {participants: user_id}}).exec(next);
 };
 
 module.exports.fetchParticipatedEvents = (user_id, next) => {
@@ -52,7 +52,7 @@ module.exports.fetchParticipatedEvents = (user_id, next) => {
 };
 
 module.exports.addFaqs = (event_id, faqs, next) => {
-    Event.findOneAndUpdate({_id: event_id}, {$push: {faqs: {$each: faqs}}}).exec(next);
+    Event.findOneAndUpdate({eventId: event_id}, {$push: {faqs: {$each: faqs}}}).exec(next);
 };
 
 module.exports.addASingleFAQ = (event_id, question, answer, next) => {
@@ -60,11 +60,11 @@ module.exports.addASingleFAQ = (event_id, question, answer, next) => {
         question: question,
         answer: answer
     };
-    Event.findOneAndUpdate({_id: event_id}, {$push: {faqs: faq}}).exec(next);
+    Event.findOneAndUpdate({eventId: event_id}, {$push: {faqs: faq}}).exec(next);
 };
 
 module.exports.fetchFaqs = (event_id, next) => {
-    Event.findOne({_id: event_id}, 'faqs').exec(next);
+    Event.findOne({eventId: event_id}, 'faqs').exec(next);
 };
 
 module.exports.addASingleSpeaker = (event_id, name, description, img_url, next) => {
@@ -73,11 +73,11 @@ module.exports.addASingleSpeaker = (event_id, name, description, img_url, next) 
         description: description,
         image_url: img_url
     };
-    Event.findOneAndUpdate({_id: event_id}, {$push: {speakers: speaker}}).exec(next);
+    Event.findOneAndUpdate({eventId: event_id}, {$push: {speakers: speaker}}).exec(next);
 };
 
 module.exports.addSpeakers = (event_id, speakers, next) => {
-    Event.findOneAndUpdate({_id: event_id}, {$push: {speakers: {$each: speakers}}}).exec(next);
+    Event.findOneAndUpdate({eventId: event_id}, {$push: {speakers: {$each: speakers}}}).exec(next);
 };
 
 module.exports.addASingleRegFeesTotheEvent = (event_id, amount, description, next) => {
@@ -85,15 +85,15 @@ module.exports.addASingleRegFeesTotheEvent = (event_id, amount, description, nex
         amount: amount,
         description: description
     };
-    Event.findOneAndUpdate({_id: event_id}, {$push: {fees: fees}}).exec(next);
+    Event.findOneAndUpdate({eventId: event_id}, {$push: {fees: fees}}).exec(next);
 };
 
 module.exports.modifyAboutOfTheEvent = (event_id, about, next) => {
-    Event.findOneAndUpdate({_id: event_id}, {about: about}).exec(next);
+    Event.findOneAndUpdate({eventId: event_id}, {about: about}).exec(next);
 };
 
 module.exports.addPointOfContacts = (event_id, contacts, next) => {
-    Event.findOneAndUpdate({_id: event_id}, {$push: {pointOfContacts: {$each: contacts}}}).exec(next);
+    Event.findOneAndUpdate({eventId: event_id}, {$push: {pointOfContacts: {$each: contacts}}}).exec(next);
 };
 
 module.exports.addASinglePointOfContact = (event_id, name, email, contact_number, next) => {
@@ -102,5 +102,5 @@ module.exports.addASinglePointOfContact = (event_id, name, email, contact_number
         email: email,
         contact: contact_number
     };
-    Event.findOneAndUpdate({_id: event_id}, {$push: {pointOfContacts: poc}}).exec(next);
+    Event.findOneAndUpdate({eventId: event_id}, {$push: {pointOfContacts: poc}}).exec(next);
 };
